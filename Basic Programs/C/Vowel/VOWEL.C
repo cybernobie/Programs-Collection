809ecd5008d81a773436b2a13449a36921691955
CyberNobie <jkarts91@gmail.com>
2021-01-07 20:19:00 +0530

    Program To Check The Char Was A Vowel Or Not

--- Basic Programs/C/Vowel/VOWEL.C
+++ Basic Programs/C/Vowel/VOWEL.C
@@ -1,21 +1,17 @@
 #include<stdio.h>
 #include<conio.h>
-
-
 void main()
-
 {
 char vowel;
 printf("Enter Any Char: ");
 scanf("%c",&vowel);
-
-if (c=='a'&&c=='A')
+if (vowel=='a'||vowel=='A'||vowel=='e'||vowel=='E'||vowel=='i'||vowel=='I'||vowel=='o'||vowel=='O'||vowel=='u'||vowel=='U')
 {
-printf("%c Is a Vowel.");
+printf("%c Is a Vowel.",vowel);
 }
 else
 {
-printf("%c Is Not a Vowel");
+printf("%c Is Not a Vowel",vowel);
 }
-
+getch();
 }
